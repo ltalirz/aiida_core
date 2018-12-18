aiidateam/aiida-core

AiiDA conda environment (#2081)
* add an `environment.yml` file for installing AiiDA's dependencies using conda
* add a pre-commit hook that updates this file based on `setup_requirements.py`
* add a Travis test that tests the conda install and prohibits adding new dependencies if they are not available on conda (either the `defaults` or the `conda-forge` channel`)
* update reentry to 1.2.2, which is now available on `conda-forge`

Note: Using 'defaults' channel (anaconda channel is currently breaking installs)
https://github.com/conda/conda/issues/7872

utils/validate_pyproject.py
@@ -13,7 +13,16 @@ import os
 import sys
 import toml
 
-@click.command()
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(dir_path, os.pardir))
+import setup_requirements
+
+
+@click.group()
+def cli():
+    pass
+
+@click.command('version')
 def validate_pyproject():
     """
     Ensure that the version of reentry in setup_requirements.py and pyproject.toml are identical
@@ -21,11 +30,7 @@ def validate_pyproject():
     filename_pyproject = 'pyproject.toml'
     filename_requirements = 'setup_requirements.py'
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
     toml_file = os.path.join(dir_path, os.pardir, filename_pyproject)
-    sys.path.append(os.path.join(dir_path, os.pardir))
-
-    import setup_requirements
 
     reentry_requirement = None
 
@@ -63,5 +68,39 @@ def validate_pyproject():
         sys.exit(1)
 
 
+@click.command('conda')
+def update_environment_yml():
+    """
+    Updates environment.yml file for conda.
+    """
+    from setup_requirements import install_requires
+    import yaml
+
+    # fix incompatibilities between conda and pypi
+    replacements = {
+        'psycopg2-binary' : 'psycopg2',
+        'validate-email' : 'validate_email',
+    }
+    sep = '%'  # use something forbidden in conda package names
+    pkg_string = sep.join(install_requires)
+    for (pypi_pkg_name, conda_pkg_name) in iter(replacements.items()):
+        pkg_string = pkg_string.replace(pypi_pkg_name, conda_pkg_name)
+    install_requires = pkg_string.split(sep)
+    environment = dict(
+        name = 'aiida',
+        channels = ['defaults', 'conda-forge', 'etetoolkit'],
+        dependencies = install_requires,
+    )
+
+    environment_filename = 'environment.yml'
+    file_path = os.path.join(dir_path, os.pardir, environment_filename)
+    with open(file_path, 'w') as env_file:
+        env_file.write('# Usage: conda env create -f environment.yml\n')
+        yaml.dump(environment, env_file, explicit_start=True, 
+                  default_flow_style=False)
+
+cli.add_command(validate_pyproject)
+cli.add_command(update_environment_yml)
+
 if __name__ == '__main__':
-    validate_pyproject()  # pylint: disable=no-value-for-parameter
+    cli()  # pylint: disable=no-value-for-parameter

ltalirz/aiida_core

utils/validate_pyproject.py
@@ -17,20 +17,33 @@ import sys
 import toml
 import json
 
-@click.command()
+THIS_DIR = os.path.split(os.path.realpath(__file__))[0]
+ROOT_DIR = os.path.join(THIS_DIR, os.pardir)
+
+FILENAME_TOML = 'pyproject.toml'
+PATH_TOML = os.path.join(ROOT_DIR, FILENAME_TOML)
+
+FILENAME_REQUIREMENTS = 'setup.json'
+PATH_REQUIREMENTS = os.path.join(ROOT_DIR, FILENAME_REQUIREMENTS)
+
+FILENAME_ENVIRONMENT = 'environment.yml'
+PATH_ENVIRONMENT = os.path.join(ROOT_DIR, FILENAME_ENVIRONMENT)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+@click.group()
+def cli():
+    pass
+
+@click.command('version')
 def validate_pyproject():
     """
     Ensure that the version of reentry in setup.json and pyproject.toml are identical
     """
-    filename_pyproject = 'pyproject.toml'
-    filename_requirements = 'setup.json'
-
-    this_path = os.path.split(os.path.realpath(__file__))[0]
-    root_dir = os.path.join(this_path, os.pardir)
-    toml_file = os.path.join(root_dir, filename_pyproject)
 
     reentry_requirement = None
-    with open(os.path.join(root_dir, 'setup.json'), 'r') as info:
+    with open(PATH_REQUIREMENTS, 'r') as info:
         setup_json = json.load(info)
 
     for requirement in setup_json['install_requires']:
@@ -39,33 +52,70 @@ def validate_pyproject():
             break
 
     if reentry_requirement is None:
-        click.echo('Could not find the reentry requirement in {}'.format(filename_requirements), err=True)
+        click.echo('Could not find the reentry requirement in {}'.format(FILENAME_REQUIREMENTS), err=True)
         sys.exit(1)
 
     try:
-        with open(toml_file, 'r') as handle:
+        with open(PATH_TOML, 'r') as handle:
             toml_string = handle.read()
     except IOError as exception:
-        click.echo('Could not read the required file: {}'.format(toml_file), err=True)
+        click.echo('Could not read the required file: {}'.format(PATH_TOML), err=True)
         sys.exit(1)
 
     try:
         parsed_toml = toml.loads(toml_string)
     except Exception as exception:
-        click.echo('Could not parse {}: {}'.format(toml_file, exception), err=True)
+        click.echo('Could not parse {}: {}'.format(PATH_TOML, exception), err=True)
         sys.exit(1)
 
     try:
         pyproject_toml_requires = parsed_toml['build-system']['requires']
     except KeyError as exception:
-        click.echo('Could not retrieve the build-system requires list from {}'.format(toml_file), err=True)
+        click.echo('Could not retrieve the build-system requires list from {}'.format(PATH_TOML), err=True)
         sys.exit(1)
 
     if reentry_requirement not in pyproject_toml_requires:
         click.echo('Reentry requirement from {} {} is not mirrored in {}'.format(
-            filename_requirements, reentry_requirement, toml_file), err=True)
+            FILENAME_REQUIREMENTS, reentry_requirement, PATH_TOML), err=True)
         sys.exit(1)
 
 
+@click.command('conda')
+def update_environment_yml():
+    """
+    Updates environment.yml file for conda.
+    """
+    import yaml
+
+    with open(PATH_REQUIREMENTS, 'r') as info:
+        setup_json = json.load(info)
+
+    install_requires = setup_json['install_requires']
+
+    # fix incompatibilities between conda and pypi
+    replacements = {
+        'psycopg2-binary' : 'psycopg2',
+        'validate-email' : 'validate_email',
+    }
+    sep = '%'  # use something forbidden in conda package names
+    pkg_string = sep.join(install_requires)
+    for (pypi_pkg_name, conda_pkg_name) in iter(replacements.items()):
+        pkg_string = pkg_string.replace(pypi_pkg_name, conda_pkg_name)
+    install_requires = pkg_string.split(sep)
+
+    environment = dict(
+        name = 'aiida',
+        channels = ['defaults', 'conda-forge', 'etetoolkit'],
+        dependencies = install_requires,
+    )
+
+    with open(PATH_ENVIRONMENT, 'w') as env_file:
+        env_file.write('# Usage: conda env create -f environment.yml\n')
+        yaml.dump(environment, env_file, explicit_start=True, 
+                  default_flow_style=False)
+
+cli.add_command(validate_pyproject)
+cli.add_command(update_environment_yml)
+
 if __name__ == '__main__':
-    validate_pyproject()  # pylint: disable=no-value-for-parameter
+    cli()  # pylint: disable=no-value-for-parameter