a65bcc6a7e489327703e492fa9b22da8aa33d865
Leopold Talirz <leopold.talirz@gmail.com>
2019-04-16 23:47:02 +0200

    rewrite quicksetup testcase to use fixtures

--- .ci/test_quicksetup.py
+++ .ci/test_quicksetup.py
@@ -37,12 +37,38 @@ from aiida.backends.tests.utils.configuration import create_mock_profile, with_t
 from aiida.cmdline.commands.cmd_setup import quicksetup
 
 
-class QuicksetupTestCase(unittest.TestCase):
-    """Test `verdi quicksetup`."""
+class ProfileManageTestCase(unittest.TestCase):
+    """Tests that manipulate profiles and their databases.
+
+     Testing:
+      * `verdi quicksetup`
+      * `verdi profile delete`
+
+      """
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up database, profile and file repository."""
+        from aiida.manage.fixtures import _GLOBAL_FIXTURE_MANAGER
+
+        cls.backend = os.environ.get('TEST_AIIDA_BACKEND', 'django')
+
+        cls.fixture_manager = _GLOBAL_FIXTURE_MANAGER
+        if cls.fixture_manager.has_profile_open():
+            raise ValueError("Fixture mananger already has an open profile. This is unexpected.")
+
+        cls.fixture_manager.backend = cls.backend
+        cls.fixture_manager.create_profile()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.fixture_manager.destroy_all()
 
     def setUp(self):
         self.runner = CliRunner()
-        self.backend = os.environ.get('TEST_AIIDA_BACKEND', 'django')
+
+    def tearDown(self):
+        self.fixture_manager.reset_db()
 
     @with_temporary_config_instance
     def test_quicksetup(self):
@@ -50,10 +76,10 @@ class QuicksetupTestCase(unittest.TestCase):
         from aiida.manage import configuration
         configuration.BACKEND_UUID = None
         result = self.runner.invoke(quicksetup, [
-            '--profile', 'giuseppe-{}'.format(self.backend), '--db-backend={}'.format(
-                self.backend), '--email=giuseppe.verdi@ope.ra', '--first-name=Giuseppe', '--last-name=Verdi',
-            '--institution=Scala', '--db-name=aiida_giuseppe_{}'.format(
-                self.backend), '--repository=aiida_giuseppe_{}'.format(self.backend), '--non-interactive'
+            '--profile=giuseppe-{}'.format(self.backend), '--db-backend={}'.format(self.backend),
+            '--email=giuseppe.verdi@ope.ra', '--first-name=Giuseppe', '--last-name=Verdi', '--institution=Scala',
+            '--db-name=aiida_giuseppe_{}'.format(self.backend), '--repository=aiida_giuseppe_{}'.format(
+                self.backend), '--non-interactive', '--db-port={}'.format(self.fixture_manager.db_port)
         ])
         self.assertFalse(result.exception, msg=get_debug_msg(result))
 
@@ -61,64 +87,44 @@ class QuicksetupTestCase(unittest.TestCase):
     def test_postgres_failure(self):
         """Test `verdi quicksetup` non-interactively with incorrect database connection parameters."""
         result = self.runner.invoke(quicksetup, [
-            '--profile', 'giuseppe2-{}'.format(self.backend), '--db-backend={}'.format(
+            '--profile=giuseppe2-{}'.format(self.backend), '--db-backend={}'.format(
                 self.backend), '--email=giuseppe2.verdi@ope.ra', '--first-name=Giuseppe', '--last-name=Verdi',
             '--institution=Scala', '--db-port=1111', '--db-name=aiida_giuseppe2_{}'.format(self.backend),
             '--repository=aiida_giuseppe2_{}'.format(self.backend), '--non-interactive', '--non-interactive'
         ])
         self.assertFalse(result.exception, msg=get_debug_msg(result))
 
-
-class DeleteTestCase(unittest.TestCase):
-    """Test `verdi profile delete`."""
-
-    def setUp(self):
-        self.runner = CliRunner()
-        self.backend = os.environ.get('TEST_AIIDA_BACKEND', 'django')
-        self.profile_list = ['mock_profile1', 'mock_profile2', 'mock_profile3', 'mock_profile4']
-
-    def mock_profiles(self):
-        """Create mock profiles and a runner object to invoke the CLI commands.
-
-        Note: this cannot be done in the `setUp` or `setUpClass` methods, because the temporary configuration instance
-        is not generated until the test function is entered, which calls the `with_temporary_config_instance`
-        decorator.
-        """
-        from aiida.manage.configuration import get_config
-
-        config = get_config()
-
-        for profile_name in self.profile_list:
-            profile = create_mock_profile(profile_name)
-            config.add_profile(profile)
-
-        config.set_default_profile(self.profile_list[0], overwrite=True).store()
-
     @with_temporary_config_instance
     def test_delete(self):
         """Test for verdi profile delete command."""
         from aiida.cmdline.commands.cmd_profile import profile_delete, profile_list
+        from aiida.manage.configuration import get_config
 
-        self.mock_profiles()
+        # Create mock profiles
+        profile_list = ['mock_profile1', 'mock_profile2', 'mock_profile3', 'mock_profile4']
+        config = get_config()
+        for profile_name in profile_list:
+            config.add_profile(create_mock_profile(profile_name))
+        config.set_default_profile(profile_list[0], overwrite=True).store()
 
         # Delete single profile
-        result = self.runner.invoke(profile_delete, ['--force', self.profile_list[1]])
+        result = self.runner.invoke(profile_delete, ['--force', profile_list[1]])
         self.assertIsNone(result.exception, result.output)
 
         result = self.runner.invoke(profile_list)
         self.assertIsNone(result.exception, result.output)
 
-        self.assertNotIn(self.profile_list[1], result.output)
+        self.assertNotIn(profile_list[1], result.output)
         self.assertIsNone(result.exception, result.output)
 
         # Delete multiple profiles
-        result = self.runner.invoke(profile_delete, ['--force', self.profile_list[2], self.profile_list[3]])
+        result = self.runner.invoke(profile_delete, ['--force', profile_list[2], profile_list[3]])
         self.assertIsNone(result.exception, result.output)
 
         result = self.runner.invoke(profile_list)
         self.assertIsNone(result.exception, result.output)
-        self.assertNotIn(self.profile_list[2], result.output)
-        self.assertNotIn(self.profile_list[3], result.output)
+        self.assertNotIn(profile_list[2], result.output)
+        self.assertNotIn(profile_list[3], result.output)
         self.assertIsNone(result.exception, result.output)
 
 
