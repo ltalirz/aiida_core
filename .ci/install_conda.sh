8dceac54d669ebb8b0a11dfed8dede73adbcb1bb
Leopold Talirz <leopold.talirz@gmail.com>
2019-10-16 19:42:51 +0200

    fix conda installation on travis

--- .ci/install_conda.sh
+++ .ci/install_conda.sh
@@ -9,6 +9,7 @@ bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
 conda config --set always_yes yes --set changeps1 no
-conda update -q conda
+# --force-reinstall flag needed, see https://superuser.com/a/1407358/345438
+conda update -q --force-reinstall conda
 # Useful for debugging any issues with conda
 conda info -a
