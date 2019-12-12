2deb8122a1d5240217e996086fecb9a29799a369
Leopold Talirz <leopold.talirz@gmail.com>
2019-12-12 16:25:59 +0100

    add default user if it does not exist

--- conftest.py
+++ conftest.py
@@ -12,3 +12,21 @@
 import pytest  # pylint: disable=unused-import
 
 pytest_plugins = ['aiida.manage.tests.pytest_fixtures']  # pylint: disable=invalid-name
+
+
+@pytest.fixture(scope='session', autouse=True)
+def default_user(self):
+    """ Add default user if required.
+
+    Note: AiiDA databases set up by the TestManager already contain a default user.
+    However, AiiDA test_profile databases *after* running tests can lack the default user.
+    """
+    from aiida.manage.configuration import get_config
+    from aiida import orm
+    from aiida.common import exceptions
+    self.user_email = get_config().current_profile.default_user
+
+    try:
+        self.user = orm.User.objects.get(email=self.user_email)
+    except exceptions.NotExistent:
+        self.user = orm.User(email=self.user_email).store()
